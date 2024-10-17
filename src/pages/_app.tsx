77d008ad527cd36bd22e34a95a38dd093b2ea172
Gabriel Medina <rha7.com@gmail.com>
2024-10-16 19:44:06 -0600

    Enabled i18next. (#2)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import '@/styles/globals.scss';
-import { store } from '@/store/store';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.scss";
+import { store } from "@/store/store";
+import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
 
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
-}
+};
+
+export default appWithTranslation(App);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
 export default function Index() {
-  return <div>Hello World!</div>;
+  const { t } = useTranslation();
+  return (
+    <div className={"container mx-auto my-8"}>
+      <div className={"prose max-w-none"}>
+        <h1>{t("index.title")}</h1>
+        <p>{t("index.description")}</p>
+      </div>
+    </div>
+  );
 }
