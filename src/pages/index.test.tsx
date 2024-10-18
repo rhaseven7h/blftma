ff00da5c941fdacb32343ed95375ce010288a652
Gabriel Medina <rha7.com@gmail.com>
2024-10-18 06:26:36 -0600

    Added full test basic implementation.

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -3,13 +3,17 @@ import { store } from '@/store/store';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
+// TODO: Refactor this onto reusable utility functions and components.
+
+// noinspection JSUnusedGlobalSymbols
 jest.mock('next-i18next', () => ({
   __esModule: true,
   useTranslation: () => {
+    // noinspection JSUnusedLocalSymbols
     return {
       t: (key: string) => key,
       i18n: {
-        getFixedT: (ns: string, lng: string, key: string) => (key: string) => (key)
+        getFixedT: (_ns: string, _lng: string, _key: string) => (key: string) => (key)
       }
     };
   }
@@ -23,16 +27,28 @@ const IndexWithStore = () => {
   );
 };
 
-global.fetch = jest.fn(() =>
-  Promise.resolve<Response>(
-    new Response(JSON.stringify({}), { status: 200 })
-  ) as Promise<Response>
-);
+const mockFetchBaseQuery = jest.fn();
+
+jest.mock('@reduxjs/toolkit/query/react', () => {
+  const originalModule = jest.requireActual('@reduxjs/toolkit/query/react');
+  // noinspection JSUnusedGlobalSymbols
+  return {
+    __esModule: true,
+    ...originalModule,
+    fetchBaseQuery: () => jest
+      .fn()
+      .mockImplementation(
+        () => mockFetchBaseQuery
+      )
+  };
+});
 
 describe('Index', () => {
   it('should render', () => {
-    const { getByText } = render(<IndexWithStore/>);
+    mockFetchBaseQuery.mockReturnValueOnce({ data: { accounts: [] } });
+    const { getByText, container } = render(<IndexWithStore/>);
     const linksParagraph = getByText('Pages Links');
+    expect(container).toMatchSnapshot();
     expect(linksParagraph).toBeInTheDocument();
   });
 });
